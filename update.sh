708fa3ff23baef7902eb1204f44f90da76083394
Ludovic Balogh <lbalogh@gmail.com>
2015-09-16 18:07:54 +0200

    Auto push updated filter list

--- update.sh
+++ update.sh
@@ -4,5 +4,6 @@ curl -o easylist.txt https://easylist-downloads.adblockplus.org/easylist.txt
 node abp2blocklist.js < easylist.txt > easylist.json
 rm -rf easylist.txt
 
-git commit -a -m "Update easyList `date`"
+git add easylist.json
+git commit -m "Update easyList `date`"
 git push origin compatible
